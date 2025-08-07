9470e7340ab99928677396b0236d987ae3983924
ThanhXT2002 <tranxuanthanhtxt2002@gmail.com>
2025-08-07 07:44:05 +0700

    Update font imports in angular.json for improved performance and remove redundant imports from styles.scss

--- src/app/components/service-section/service-section.ts
+++ src/app/components/service-section/service-section.ts
@@ -1,13 +1,11 @@
 import { Component } from '@angular/core';
-import { SectionLabel } from "../section-label/section-label";
 import { CommonModule } from '@angular/common';
-import { IconBoxWrapper } from "../icon-box-wrapper/icon-box-wrapper";
 import { ProductList } from "../product-list/product-list";
 import { SectionIntro } from "../section-intro/section-intro";
 
 @Component({
   selector: 'app-service-section',
-  imports: [SectionLabel, CommonModule, ProductList, SectionIntro],
+  imports: [ CommonModule, ProductList, SectionIntro],
   templateUrl: './service-section.html',
   styleUrl: './service-section.scss'
 })
