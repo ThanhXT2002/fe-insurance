78851337ef11f56267bcb69c2962f13ef2833437
ThanhXT2002 <tranxuanthanhtxt2002@gmail.com>
2025-09-14 05:45:35 +0700

    feat: update environment configuration to remove Firebase and integrate Supabase

--- generate-env.js
+++ generate-env.js
@@ -11,17 +11,10 @@ const devEnvContent = `export const environment = {
   production: false,
   apiUrl: '${process.env.DEV_API_URL || ''}',
   appVersion: require('../../package.json').version,
-  firebaseConfig: {
-    apiKey: '${process.env.FIREBASE_API_KEY || ''}',
-    authDomain: '${process.env.FIREBASE_AUTH_DOMAIN || ''}',
-    projectId: '${process.env.FIREBASE_PROJECT_ID || ''}',
-    storageBucket: '${process.env.FIREBASE_STORAGE_BUCKET || ''}',
-    messagingSenderId: '${process.env.FIREBASE_MESSAGING_SENDER_ID || ''}',
-    appId: '${process.env.FIREBASE_APP_ID || ''}',
-    measurementId: '${process.env.FIREBASE_MEASUREMENT_ID || ''}',
-  },
+  SUPABASE_URL: '${process.env.SUPABASE_URL || ''}',
+  SUPABASE_ANON_KEY: '${process.env.SUPABASE_ANON_KEY || ''}',
   appName: 'XTBH',
-  emailSupport: 'xtbh@gmail.com',
+  emailSupport: 'xtbaohieninfo@gmail.com',
   fbAppId: '${process.env.FB_APP_ID || ''}',
 };`;
 
@@ -30,17 +23,10 @@ const prodEnvContent = `export const environment = {
   production: true,
   apiUrl: '${process.env.API_URL || ''}',
   appVersion: require('../../package.json').version,
-  firebaseConfig: {
-    apiKey: '${process.env.FIREBASE_API_KEY || ''}',
-    authDomain: '${process.env.FIREBASE_AUTH_DOMAIN || ''}',
-    projectId: '${process.env.FIREBASE_PROJECT_ID || ''}',
-    storageBucket: '${process.env.FIREBASE_STORAGE_BUCKET || ''}',
-    messagingSenderId: '${process.env.FIREBASE_MESSAGING_SENDER_ID || ''}',
-    appId: '${process.env.FIREBASE_APP_ID || ''}',
-    measurementId: '${process.env.FIREBASE_MEASUREMENT_ID || ''}',
-  },
+  SUPABASE_URL: '${process.env.SUPABASE_URL || ''}',
+  SUPABASE_ANON_KEY: '${process.env.SUPABASE_ANON_KEY || ''}',
   appName: 'XTBH',
-  emailSupport: 'xtbh@gmail.com',
+  emailSupport: 'xtbaohieninfo@gmail.com',
   fbAppId: '${process.env.FB_APP_ID || ''}',
 };`;
 
